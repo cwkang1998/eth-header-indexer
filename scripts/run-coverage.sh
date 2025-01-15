90fab8a595ea15be6f86c853fca0d98e4ecf8112
Chen Wen Kang <23054115+cwkang1998@users.noreply.github.com>
2025-01-15 17:45:57 +0800

    chore: setup ci for testing with docker compose (#29)

---
+++ scripts/run-coverage.sh
@@ -0,0 +1,6 @@
+export TEST_DB_CONNECTION_STRING="postgresql://postgres:postgres@localhost:5433/fossil_test"
+
+# Run tests with coverage using cargo-llvm-cov.
+# Note that you will have to install the tarpaulin binary via cargo install cargo-llvm-cov.
+cargo llvm-cov --all-features --workspace --lcov --output-path lcov.info
+
